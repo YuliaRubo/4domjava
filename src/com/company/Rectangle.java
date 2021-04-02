39c15a4be7f00b5c0e25d2a01c37e70832a3303e
Ulya Ivancova <ulya-rubo@mail.ru>
2021-04-02 14:19:07 +0200

    commit

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Rectangle {
+    protected double x;
+    protected double y;
+
+    public Rectangle(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Rectangle(double x) {
+        this.x = x;
+        this.y = x;
+    }
+
+    public double calculateArea() {
+        return this.x*this.y;
+
+    }
+
+
+    public void printArea() {
+        System.out.println("Площадь " + setRectangleKind() + " " + calculateArea());
+    }
+
+    public void printRectangleKind() {
+        if (this.x == this.y) {
+            System.out.println("Это квадрат");
+        } else {
+            System.out.println("Это прямоугольник");
+        }
+    }
+
+    public String setRectangleKind() {
+        if (this.x == this.y) {
+            return "квадратa";
+        } else {
+            return "прямоугольникa";
+        }
+    }
+
+    public boolean isTheSameRectangle(Rectangle R2) {
+        if (R2.x==this.x & R2.y==this.y ) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+}
